bd7acb973425f65e03c19feb1e96721049075891
u7chan <t.unagami.dev@gmail.com>
2024-05-08 22:59:44 +0900

    fix: add Return JSON example

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,11 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
+app.get("/api/hello", (c) => {
+	return c.json({
+		ok: true,
+		message: "Hello Hono!",
+	});
+});
+
 export default app;
