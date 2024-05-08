6ce436b1b067428f537b802ba8fc1a2d72df0cd2
u7chan <t.unagami.dev@gmail.com>
2024-05-08 23:05:15 +0900

    refactor: rename endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+app.get("/api/health_check", (c) => {
+	return c.text("OK");
 });
 
-app.get("/api/hello", (c) => {
+app.get("/api/json_example", (c) => {
 	return c.json({
 		ok: true,
 		message: "Hello Hono!",
