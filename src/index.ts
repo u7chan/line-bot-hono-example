e71fc4fbece84b857eb198f211b104d11154b761
u7chan <t.unagami.dev@gmail.com>
2024-05-09 23:55:14 +0900

    fix: add formatter options for biome

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,25 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/api/health_check", (c) => {
-	return c.text("OK");
+  return c.text("OK");
 });
 
 app.get("/api/json_example", (c) => {
-	return c.json({
-		ok: true,
-		message: "Hello Hono!",
-	});
+  return c.json({
+    ok: true,
+    message: "Hello Hono!",
+  });
 });
 
 app.post("/api/webhook", async (c) => {
-	const contentType = c.req.header("Content-Type");
-	if (!contentType?.includes("application/json")) {
-		c.status(400);
-		return c.text("Bad Request");
-	}
-	const data = await c.req.json();
-	console.log("#data", data);
-	return c.text("OK");
+  const contentType = c.req.header("Content-Type");
+  if (!contentType?.includes("application/json")) {
+    c.status(400);
+    return c.text("Bad Request");
+  }
+  const data = await c.req.json();
+  console.log("#data", data);
+  return c.text("OK");
 });
 
 export default app;
